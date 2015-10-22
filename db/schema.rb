4aa7bd7da199f571a4b033c14c8984218850b9e8
mperk <P_E_R_K@hotmail.com>
2015-10-22 10:24:22 +0300

    Lecture 15 finish

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151021090025) do
+ActiveRecord::Schema.define(version: 20151022063957) do
+
+  create_table "movies", force: :cascade do |t|
+    t.string   "name"
+    t.string   "director"
+    t.text     "description"
+    t.integer  "year"
+    t.decimal  "length"
+    t.string   "format"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "people", force: :cascade do |t|
     t.string   "first_name"
