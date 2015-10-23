7f8f26ef054304b47afdbb4f4d7156340ff4f7be
mperk <P_E_R_K@hotmail.com>
2015-10-23 10:27:17 +0300

    Lecture 16 Finish

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,6 @@
 class Movie < ActiveRecord::Base
+  validates_presence_of :name, :director
+  validates_numericality_of :year, :length, :greater_than => 0 #zorunlu olmasını sağlıyor
+  validates_uniqueness_of :name, :message => "Already Used, Stupid" #Uniq olmasını sağlıyor
+  validates_length_of :format, :maximum => 5, :minimum =>  2
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151022063957) do
+ActiveRecord::Schema.define(version: 20151023071341) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "name"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20151022063957) do
     t.string   "format"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "image"
   end
 
   create_table "people", force: :cascade do |t|
